74aea3903b7d93e3b14d643a1a9e6366e2a4312c
abood.jarrar <abd.a.jarrar@gmail.com>
2026-08-10 02:18:32 +0300

    finalze building the model classes

---
+++ Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystemProject2.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystemProject2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int DepartmentId { get; set; }
+        public DateOnly HireDate { get; set; }
+        public decimal Salary { get; set; }
+        public List<string> Skills { get; set; } = new List<string>();
+
+    }
+}
